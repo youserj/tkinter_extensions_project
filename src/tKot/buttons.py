827a79ab62b7124f06662901e8ff3ed5cbc240e3
vasilsv <oe8v3s>
2023-02-27 16:41:26 +0300

    ver 0.0.4 +Icon: arg info

--- src/tKot/buttons.py
+++ src/tKot/buttons.py
@@ -5,12 +5,15 @@ from PIL import Image, ImageTk
 class Icon(tk.Button):
     p_im: Image.Image | None
     im: ImageTk.PhotoImage | None
+    info: str | None
 
     def __init__(self, pillow_image: Image = None,
+                 info: str = None,
                  **kwargs):
         super(Icon, self).__init__(**kwargs)
         self.p_im = pillow_image
         self.im = None
+        self.info = info
         self.bind("<Configure>", self.__handle_configure)
 
     def __handle_configure(self, e: tk.Event):
