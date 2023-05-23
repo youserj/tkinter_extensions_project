0f591d3173fb38a689a45a6b39c3c6cb9b0f3f8d
vasilsv <oe8v3s>
2023-05-23 10:54:22 +0300

    ver 0.3.3 * Point: __mul__

--- src/tKot/common.py
+++ src/tKot/common.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Self
 
 
 @dataclass
@@ -17,4 +18,11 @@ class Point:
     def __str__(self):
         return F'<{self.x},{self.y}>'
 
-
+    def __mul__(self, other: int | float) -> Self:
+        """:return Point with scaler x and y by other value"""
+        if isinstance(other, (int, float)):
+            self.x = self.x*other
+            self.y = self.y*other
+            return self
+        else:
+            ValueError(F"got unsupport type: {other}, expected int or float")
