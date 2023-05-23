e7eee58c361382ffa8ab1dc18d6ebd7ea7b7e048
vasilsv <oe8v3s>
2023-05-23 09:06:45 +0300

    ver 0.3.0 + StatusIcon for canvas

---
+++ examples/statusIcon.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from src.tKot.buttons import StatusIcon
+from PIL import Image
+
+
+root = tk.Tk()
+c = tk.Canvas(root)
+c.pack()
+on_image = Image.open("../data/relay_on2.png")
+off_image = Image.open("../data/relay_off2.png")
+s_icon = StatusIcon(c, {1: on_image, 2: off_image})
+s_icon.place(10, 10)
+root.after(2000, lambda: s_icon.set_status(2))
+root.after(4000, lambda: s_icon.set_status(1))
+root.mainloop()
--- src/tKot/buttons.py
+++ src/tKot/buttons.py
@@ -27,6 +27,7 @@ class Icon(tk.Widget):
 
 
 class StatusIcon:
+    """Icon with status properties"""
     __c: tk.Canvas
     c_id: int
     __current: int
@@ -34,7 +35,10 @@ class StatusIcon:
     __h: int
     __w: int
 
-    def __init__(self, canvas: tk.Canvas, images: dict[int, Image]):
+    def __init__(self,
+                 canvas: tk.Canvas,
+                 images: dict[int, Image],
+                 default: int = None):
         self.__c = canvas
         """ canvas for widget """
         self.__c_id = -1
@@ -45,28 +49,29 @@ class StatusIcon:
         """icon height"""
         self.__w = 100
         """icon width"""
+        self.set_status(tuple(self.__images.keys())[0] if default is None else self.__images[default])
 
     def get_status(self) -> int:
+        """return current status"""
         return self.__current
 
     def set_status(self, value: int):
+        """set status with change image"""
         self.__im = ImageTk.PhotoImage(self.__images[value].resize((self.__h, self.__w)))
-        self.__c.create_image()
         self.__current = value
+        if self.__c_id != -1:
+            self.__c.itemconfigure(self.__c_id, image=self.__im)
+
+    def set_width(self, value: int):
+        self.__w = value
+
+    def set_height(self, value: int):
+        self.__h = value
 
     def place(self, x: int, y: int):
-        """ place on canvas by x and y """
-        self.__coords = x, y
-        """ x, y image coordinates """
-        if self.__c != -1:
+        """ replace on canvas by (x, y). Old canvas id shall delete"""
+        if self.__c_id != -1:
             self.__c.delete(self.__c_id)
-        # self.__c.create_rectangle(x + self.__shadow_depth, y + self.__shadow_depth,
-        #                                x + self.__size + self.__shadow_depth, y + self.__size + self.__shadow_depth,
-        #                                width=0,
-        #                                fill='grey')
-        # self.__c_id = self.__c.create_image(x + self.__shadow_depth * int(self.__status), y + self.__shadow_depth * int(self.__status),
-        #                                     anchor=tk.NW,
-        #                                     image=self.__tk_image)
         self.__c_id = self.__c.create_image(x, y,
                                             anchor=tk.NW,
                                             image=self.__im)
